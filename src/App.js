c9cbf88b91365b6090279c51240f04ba3adb16a7
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-04 14:33:14 -0700

    refactor: renders data from localhost 8080

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 // import { useState, useEffect } from 'react';
 import RenderForm from './components/RenderForm';
-
-// import axios from 'axios';
 // import InputForm from './components/InputForm';
-
-
-// const axios = require('axios').default;
-// const axiosInstance = axios.create({
-//   baseURL: process.env.REACT_APP_API_DOMAIN
-// });
-// axiosInstance.defaults.headers.common['Authorization'] = process.env.REACT_APP_PAT;
-
-
-// const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
-// const PAT = process.env.REACT_APP_PAT;
 
 function App() {
   
--- src/components/RenderForm.js
+++ src/components/RenderForm.js
@@ -1,63 +1,32 @@
-import metadata from '../metadata';
-import RenderRow from './RenderRow';
+import { useState, useEffect } from 'react';
 
-
-function RenderForm(props) {
+function RenderForm() {
   /**
    * Read user input from InputForm component.
    * Perform GET request based on user input
    * Render response from GET request
    * 
-   * @param {string} props.userInput is passed in and used for GET request
+   * @param {string} userInput is passed in and used for GET request
    * @return {string} JSON object is returned as a string
+   * url = http://localhost:8080/data_assets
    */
+  
+  const [schema, setSchema] = useState();
+  
+  const URL = 'http://localhost:8080/data_assets';
 
-  const getKeys = () => {
-    return Object.keys(metadata.results[0])
-  }
+  useEffect(() => {
 
-  const getHeader = () => {
-    let keys = getKeys();
-
-    return keys.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>
-    })
-  }
-
-  const rowData = () => {
-    let items = metadata.results;
-    let keys = getKeys();
-
-    // Creating a deep copy of the data
-    const itemsCopy = JSON.parse(JSON.stringify(items))
-
-    // Converting UNIX timestamp
-    itemsCopy.forEach(curObj => {
-      curObj.created = new Date(curObj.created * 1000).toLocaleString();
-
-      itemsCopy.push(curObj.created)
+    fetch(URL)
+    .then(results => results.json())
+    .then(data => {
+      setSchema(data);
     });
+  }, [])
 
-    return itemsCopy.map((row, index) => {
-      return (
-      <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
-      )
-    })
-  }
-
-
-    return (
-      <table>
-        <thead>
-          <tr>
-          {getHeader()}
-          </tr>
-        </thead>
-        <tbody>
-          {rowData()}
-        </tbody>
-      </table>
-    )
+  return (
+    <pre id="json">{JSON.stringify(schema, null, 4)}</pre>
+  );
 };
 
 export default RenderForm;
