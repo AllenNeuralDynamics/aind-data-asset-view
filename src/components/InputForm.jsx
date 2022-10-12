84914d1f4fec08f6b992fa51af1638c2c2cd4a51
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-12 16:48:06 -0700

    feat: adds dropdowns for sort field and sort order

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const sortFieldOptions = ['Created', 'Type', 'Name', 'Size'];
+
 function InputForm({ handleData }) {
   /**
    * Function to read user input from form submit, update state, and pass to parent component.
@@ -9,6 +11,8 @@ function InputForm({ handleData }) {
    */
 
   const [userInput, setUserInput] = useState('');
+  const [selectedField, setSelectedField] = useState(sortFieldOptions[0]);
+  const [sortBy, setSortBy] = useState('');
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
@@ -20,10 +24,26 @@ function InputForm({ handleData }) {
     handleData(userInput);
   };
 
+  const sortSubmit = () => {
+    console.log(selectedField);
+    console.log(sortBy);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" value={userInput} onChange={handleChange} />
-      <button type="submit" onClick={handleSubmit}>
+      {/* Sort By dropdown */}
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
+      </select>
+      {/* Sort Field dropdown */}
+      <select value={selectedField} onChange={(e) => setSelectedField(e.target.value)}>
+        {sortFieldOptions.map((value) => (
+          <option value={value} key={value}>{value}</option>
+        ))}
+      </select>
+      <button type="submit" onClick={sortSubmit}>
         Submit
       </button>
     </form>
