fb9952d0b7cb27a625d9ef2c6063e11beb5e224f
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-14 14:43:14 -0700

    refactor: adds default parameters

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 const sortFieldOptions = ['Created', 'Type', 'Name', 'Size'];
 
+const defaultParams = {
+  start: 0,
+  limit: 10,
+  sort_field: '',
+  sort_order: '',
+  type: '',
+};
+
 function InputForm({ handleData }) {
   /**
    * Function to read user input from form submit, update state, and pass to parent component.
@@ -10,12 +18,10 @@ function InputForm({ handleData }) {
    * @return {string} userInput
    */
 
-  const [userInput, setUserInput] = useState('');
-  const [selectedField, setSelectedField] = useState(sortFieldOptions[0]);
-  const [sortBy, setSortBy] = useState('asc');
+  const [userInput, setUserInput] = useState(defaultParams);
 
   const handleChange = (event) => {
-    setUserInput(event.target.value);
+    setUserInput({ ...userInput, [event.target.name]: event.target.value });
   };
 
   const handleSubmit = (event) => {
@@ -25,29 +31,28 @@ function InputForm({ handleData }) {
   };
 
   const sortSubmit = () => {
-    console.log(selectedField);
-    console.log(sortBy);
+    console.log('Clicked!');
   };
 
   return (
     <form onSubmit={handleSubmit}>
       {/* Type dropdown */}
-      <select value={userInput} onChange={handleChange}>
-        <option value="Result">Result</option>
-        <option value="Dataset">Dataset</option>
+      <select name="type" value={userInput.type} onChange={handleChange}>
+        <option value="result">Result</option>
+        <option value="dataset">Dataset</option>
       </select>
       {/* Start and Limit for search index */}
-      <input type="number" placeholder="Start" min="0" max="30" />
-      <input type="number" placeholder="Limit" min="0" max="30" />
+      <input name="start" value={userInput.value} type="number" placeholder="Start" min="0" max="30" onChange={handleChange} />
+      <input name="limit" value={userInput.value} type="number" placeholder="Limit" min="0" max="30" onChange={handleChange} />
       {/* Sort order dropdown */}
-      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+      <select name="sort_order" value={userInput.sort_order} onChange={handleChange}>
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
       {/* Sort Field dropdown */}
-      <select value={selectedField} onChange={(e) => setSelectedField(e.target.value)}>
+      <select name="sort_field" value={userInput.sort_field} onChange={handleChange}>
         {sortFieldOptions.map((value) => (
-          <option value={value} key={value}>{value}</option>
+          <option value={value.toLowerCase()} key={value}>{value}</option>
         ))}
       </select>
       <button type="submit" onClick={sortSubmit}>
