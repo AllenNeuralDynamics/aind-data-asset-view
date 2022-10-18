d07b081f9d66db548645d017ce74b6e0c9d63138
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-18 11:27:47 -0700

    refactor: modifies urlBuilder to use forEach

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,11 +1,9 @@
 export const urlBuilder = (urlString, userInput) => {
   const urlProxy = new URL(urlString);
-  const queryParams = new URLSearchParams(urlProxy.search);
-  for (const key in userInput) {
-    if (userInput[key] !== undefined) {
-      queryParams.append(key, userInput[key].toLowerCase());
-    }
-  }
-  urlProxy.search = queryParams.toString();
+
+  Object.entries(userInput).forEach(([key, value]) => {
+    urlProxy.searchParams.append(key, value)
+  })
+  
   return urlProxy.toString();
 };
--- src/utilities/utils.test.js
+++ src/utilities/utils.test.js
@@ -1,6 +1,6 @@
 import { urlBuilder } from './utils';
 
-test('checks url is built correctly', () => {
+test('Checks url is built correctly', () => {
   const actualURL = urlBuilder('http://localhost:8080/data_assets', { type: 'dataset' });
   const expected = 'http://localhost:8080/data_assets?type=dataset';
   expect(actualURL).toBe(expected);
