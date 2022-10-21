f9505f5a40144c6f747dabec74d1acc31018368c
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-21 13:57:48 -0700

    feat: adds unit tests to 3 of 6 inputs

--- src/__tests__/InputForm.test.jsx
+++ src/__tests__/InputForm.test.jsx
@@ -1,54 +1,65 @@
 import React from 'react';
 // import selectEvent from 'react-select-event';
-import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act, render, screen } from '@testing-library/react';
 import InputForm from '../components/InputForm';
 
-test('input form renders correctly', () => {
-  const { getByTestId } = render(<InputForm />);
 
+const setup = () => {
+  const { getByTestId, getByText, getByLabelText } = render(<InputForm />);
+  
   const buttonElement = getByTestId('submit-btn');
   const typeSelect = getByTestId('select-type');
   const orderSelect = getByTestId('select-sort-order');
   const sortFieldSelect = getByTestId('select-sort-field');
+  
+  return {buttonElement, typeSelect, orderSelect, sortFieldSelect, getByText, getByLabelText} 
+};
 
-  expect(buttonElement).toBeInTheDocument();
-  expect(typeSelect).toBeInTheDocument();
-  expect(orderSelect).toBeInTheDocument();
-  expect(sortFieldSelect).toBeInTheDocument();
+
+
+describe('test input form', () => {
+
+  test('Render input fields correctly', () => {
+    const { buttonElement, typeSelect, orderSelect, sortFieldSelect } = setup();
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(typeSelect).toBeInTheDocument();
+    expect(orderSelect).toBeInTheDocument();
+    expect(sortFieldSelect).toBeInTheDocument();
+  });
+
+  
+  test('Should display correct number of data asset type options', () => {
+    const { typeSelect } = setup();
+    expect(typeSelect.length).toBe(1);
+  });
+
+  test('Should display correct number of sort field options', () => {
+    const { sortFieldSelect } = setup();
+    expect(sortFieldSelect.length).toBe(4);
+  });
+
+  test('Show selected sort field option on click', async () => {
+    const { getByText } = setup();
+    const selectedItem = getByText('Size');
+    await act(async () => userEvent.click(selectedItem));
+    expect(getByText('Size')).toBeInTheDocument();
+  });
+  
+  test('Displays correct number of sort order options', () => {
+    const { orderSelect } = setup();
+    expect(orderSelect.length).toBe(2);
+  })
+
+  test('Show selected sort order on click', async () => {
+    const { getByText } = setup();
+    const selectedOrder = getByText('Descending');
+    await act(async () => userEvent.click(selectedOrder));
+    expect(getByText('Descending')).toBeInTheDocument();
+  })
+
+  // test submit button when clicked --> expect the response?
+  // test start
+  // test limit
 });
-
-// describe('Select data asset type', () => {
-
-//   const { getByTestId } = render(<InputForm />);
-
-//   const typeSelect = getByTestId('select-type');
-
-//   test('Should display the correct number of options', () => {
-//     expect(typeSelect.length).toBe(1);
-//   });
-// })
-
-//   test('select data asset type', () => {
-//     expect(screen.getByText('Dataset')).toBe(true);
-//   });
-// });
-
-//   // test('Should render button label correctly', () => {
-//   //   const buttonElement = screen.getByTestId('submit-btn');
-//   //   expect(buttonElement).toBeInTheDocument();
-//   //   expect(buttonElement.toHaveTextContent('Submit'));
-//   // });
-
-//   // test('Select element for data asset type', () => {
-//   //   const selectType = selectEvent.select(getByTestId('select-type'), ['dataset']);
-//   //   expect(selectType).toHaveFormValues(['dataset']);
-//   // })
-
-
-// describe('<button />', () => {
-//   test('Should render button label correctly', () => {
-//     const buttonElement = screen.getByRole('button', {name: /submit/i, hidden: true });
-//     expect(buttonElement).toBeInTheDocument();
-//     expect(buttonElement.toHaveTextContent('Submit'));
-//   });
-// });
