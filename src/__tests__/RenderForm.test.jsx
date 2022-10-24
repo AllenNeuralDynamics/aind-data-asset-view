89ce7cca8b39141e725ee9dd935d0e528e27695e
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-24 16:00:33 -0700

    refactor: attempts to mock fetch request

--- src/__tests__/RenderForm.test.jsx
+++ src/__tests__/RenderForm.test.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import RenderForm from '../components/RenderForm';
 
-describe('render form component', () => {
-  test('correctly renders renderform component', () => {
+describe('Render form component', () => {
+  test('Correctly renders renderform component', () => {
     render(<RenderForm />);
   });
-  // Mock fetch request to test
 });
+
+// const mockUserInput = {
+
+// }
+
+// const fetchMock = jest
+//   .spyOn(global, 'fetch')
+//   .mockImplementation(() => Promise.resolve({json: () => Promise.resolve([]) }));
+
+// Mock fetch request to test
+// describe('Mock fetch request to api', () => {
+//   test('Should return 10 data assets', async () => {
+//     const json = await fetchMock();
+//     expect(fetchMock).toHaveBeenCalledWith('http://localhost:8080/data_assets');
+//     expect(json.length).toEqual(10);
+//   })
+
+// });
