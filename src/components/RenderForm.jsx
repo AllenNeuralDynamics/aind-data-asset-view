2c0d92b799f9539618299d6ab8221e4c7a9e3a0e
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-10-25 16:51:34 -0700

    refactor: removes unused import

--- src/components/RenderForm.jsx
+++ src/components/RenderForm.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import urlBuilder from '../utilities/utils';
-// import getResponse from '../utilities/getResponse';
 
 function RenderForm({ userInput }) {
   /**
