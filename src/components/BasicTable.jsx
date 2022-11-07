66cee29bbd5b6c1e1f74090c0ab645dfd5cbe949
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-11-07 08:54:40 -0800

    refactor: get request to code ocean, render MUI table

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,43 +1,56 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Table, TableBody, TableCell, TableContainer,
   TableHead, TableRow, Paper,
 } from '@mui/material';
 // import urlBuilder from '../utilities/utils';
 
-function createData(number, item, qty, price) {
-  return {
-    number, item, qty, price,
-  };
-}
-
-const rows = [
-  createData(1, 'Apple', 5, 3),
-  createData(2, 'Orange', 2, 2),
-  createData(3, 'Grapes', 3, 1),
-  createData(4, 'Tomato', 2, 1.6),
-  createData(5, 'Mango', 1.5, 4),
-];
+// const rows = [];
 
 export default function DynamicTable() {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const getResponse = async () => {
+      const response = await fetch('http://localhost:8080/data_assets')
+        .catch((error) => {
+          console.log(error);
+        });
+      const data = await response.json();
+      setTableData(data.results);
+    };
+    getResponse();
+  }, []);
+
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>S.No</TableCell>
-            <TableCell align="right">Item</TableCell>
-            <TableCell align="right">Quantity&nhsp;(kg)</TableCell>
-            <TableCell align="right">Price&nbsp;($)</TableCell>
+            <TableCell align="right">Created On</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Files</TableCell>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">Last Used</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Size</TableCell>
+            <TableCell align="right">Tags</TableCell>
+            <TableCell align="right">Type</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.number}>
-              <TableCell component="th" scope="row">{row.number}</TableCell>
-              <TableCell align="right">{row.item}</TableCell>
-              <TableCell align="right">{row.qty}</TableCell>
-              <TableCell align="right">{row.price}</TableCell>
+          {tableData.map((row) => (
+            <TableRow key={row.id}>
+              {/* <TableCell component="th" scope="row">{row.id}</TableCell> */}
+              <TableCell align="right">{row.created}</TableCell>
+              <TableCell align="right">{row.description}</TableCell>
+              <TableCell align="right">{row.files}</TableCell>
+              <TableCell align="right">{row.id}</TableCell>
+              <TableCell align="right">{row.last_used}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.size}</TableCell>
+              <TableCell align="right">{row.tags}</TableCell>
+              <TableCell align="right">{row.type}</TableCell>
             </TableRow>
           ))}
         </TableBody>
