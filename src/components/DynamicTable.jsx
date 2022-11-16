fe6af973c7c83cd5eb268b3b6a1fcdd7ee2dd426
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-11-16 09:21:46 -0800

    build: adds data grid

--- src/components/DynamicTable.jsx
+++ src/components/DynamicTable.jsx
@@ -1,68 +1,61 @@
 import { useState, useEffect } from 'react';
-import {
-  Table, TableBody, TableCell, TableContainer,
-  TableHead, TableRow, Paper,
-} from '@mui/material';
-import { DateTime } from 'luxon';
+import { DataGrid } from '@mui/x-data-grid';
+// import { DateTime } from 'luxon';
 
-const headers = [
+const columns = [
   {
-    id: 1,
-    name: 'date',
+    field: 'col1',
+    headerName: 'date',
   },
   {
-    id: 2,
-    name: 'description',
+    field: 'col2',
+    headerName: 'description',
   },
   {
-    id: 3,
-    name: 'files',
+    field: 'col3',
+    headerName: 'files',
   },
   {
-    id: 4,
-    name: 'id',
+    field: 'col4',
+    headerName: 'id',
   },
   {
-    id: 5,
-    name: 'last used',
+    field: 'col5',
+    headerName: 'last used',
   },
   {
-    id: 6,
-    name: 'name',
+    field: 'col6',
+    headerName: 'name',
   },
   {
-    id: 7,
-    name: 'size',
+    field: 'col7',
+    headerName: 'size',
   },
   {
-    id: 8,
-    name: 'state',
+    field: 'col8',
+    headerName: 'state',
   },
   {
-    id: 9,
-    name: 'tags',
+    field: 'col9',
+    headerName: 'tags',
   },
   {
-    id: 10,
-    name: 'type',
-  },
-  {
-    id: 11,
-    name: 'provenance',
+    field: 'col10',
+    headerName: 'type',
   },
 ];
 
 export default function DynamicTable() {
   const [tableData, setTableData] = useState([]);
 
-  const convertTimestamp = (timeValue) => {
-    const formattedDatetime = DateTime.fromSeconds(timeValue).toLocaleString(DateTime.DATETIME_MED);
-    return formattedDatetime;
-  };
+  // const convertTimestamp = (timeValue) => {
+  //   const formattedDatetime = DateTime.fromSeconds(timeValue).toLocaleString(DateTime.DATETIME_MED);
+  //   return formattedDatetime;
+  // };
 
   useEffect(() => {
     const getResponse = async () => {
-      const response = await fetch('http://localhost:8080/data_assets')
+      const response = await fetch('http://localhost:8080/data_assets?type=dataset')
         .catch((error) => {
           console.log(error);
         });
@@ -72,29 +65,37 @@ export default function DynamicTable() {
     getResponse();
   }, []);
 
+  
+
   return (
-    <TableContainer component={Paper}>
-      <Table stickyHeader aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {headers.map((item) => (
-              <TableCell key={item.id}>{item.name.toUpperCase()}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableData.map((row) => (
-            <TableRow key={row.id}>
-              {Object.keys(row).map((key) => {
-                if (key === 'created') {
-                  return <TableCell>{convertTimestamp(row[key])}</TableCell>;
-                }
-                return <TableCell>{row[key]}</TableCell>;
-              })}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      {/* <DataGrid
+        rows={tableData}
+        columns={headers}
+      /> */}
+    </div>
+    // <TableContainer component={Paper}>
+    //   <Table stickyHeader aria-label="simple table">
+    //     <TableHead>
+    //       <TableRow>
+    //         {headers.map((item) => (
+    //           <TableCell key={item.id}>{item.name.toUpperCase()}</TableCell>
+    //         ))}
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {tableData.map((row) => (
+    //         <TableRow key={row.id}>
+    //           {Object.keys(row).map((key) => {
+    //             if (key === 'created') {
+    //               return <TableCell>{convertTimestamp(row[key])}</TableCell>;
+    //             }
+    //             return <TableCell>{row[key]}</TableCell>;
+    //           })}
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </TableContainer>
   );
 }
