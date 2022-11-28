30f5ace5746fe8ea460cf65182c2431c99ae5c15
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-11-28 13:58:33 -0800

    refactor: removes main page component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Header from './components/Header/Header';
-import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <Grid container>
       <Header />
-      <MainPage />
       <Outlet />
     </Grid>
   );
