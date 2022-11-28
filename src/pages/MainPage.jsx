acb6cfa52b8efa42eaf1e33fda673eb9d5c51bce
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-11-28 10:50:39 -0800

    refactor: updates main page component

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,95 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function MainPage() {
+const cards = [1, 2, 3];
+
+const theme = createTheme();
+
+export default function MainPage() {
   return (
-    <Grid item md={12} sx={{ marginLeft: '300px' }}>
-      <Box component="main" sx={{ flexGrow: 1, p: 9 }}>
-        <div>This is the main page.</div>
-      </Box>
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        alignItems="center"
+        spacing={0}
+        justifyContent="center"
+        direction="column"
+      >
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h2"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Allen Institute for Neural Dynamics
+              </Typography>
+              <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.
+              </Typography>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              />
+            </Container>
+          </Box>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                      // 16:9
+                        pt: '56.25%',
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+      </Grid>
+    </ThemeProvider>
   );
 }
-
-export default MainPage;
