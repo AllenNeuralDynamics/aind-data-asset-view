11cfea592138f1a8478ade83ef617afae7486699
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-12-14 11:04:07 -0800

    feat: adds textfield component

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Stack, TextField } from '@mui/material';
 
 function InputForm({ handleData }) {
   /**
@@ -8,6 +9,10 @@ function InputForm({ handleData }) {
    * @return {string} userInput
    */
 
+  useEffect(() => {
+    handleData({'type': 'both'})
+  }, [])
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -15,28 +20,29 @@ function InputForm({ handleData }) {
     handleData(formDataObject);
   };
 
-  useEffect(() => {
-    // mimic event object
-    handleData({'type': 'both'})
-  }, [])
-
   return (
-    <form onSubmit={handleSubmit} data-testid="form">
-      <select name="type" defaultValue="" data-testid="select-type">
-        <option defaultValue="both">Both</option>
-        <option value="result">Result</option>
-        <option value="dataset">Dataset</option>
-      </select>
-      <button type="submit" data-testid="submit-btn" aria-hidden="true">
-        Submit
-      </button>
-    </form>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction='row'>
+      <form onSubmit={handleSubmit} data-testid="form">
+        <select name="type" defaultValue="" data-testid="select-type">
+          <option defaultValue="both">Both</option>
+          <option value="result">Result</option>
+          <option value="dataset">Dataset</option>
+        </select>
+        <button type="submit" data-testid="submit-btn" aria-hidden="true">
+          Submit
+        </button>
+      </form>
+      <TextField label='Search all/title/author/tags' size='small' color='secondary' />
+      </Stack>
+    </Stack>
   );
 }
 
 InputForm.propTypes = {
   handleData: PropTypes.func,
 };
+
 InputForm.defaultProps = {
   handleData: undefined,
 };
