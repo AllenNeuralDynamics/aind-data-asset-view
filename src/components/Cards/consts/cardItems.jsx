8720be2c4e67162f9c644c4d7968e33da6a761fb
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-01-13 15:58:16 -0800

    feat: adds card content

--- src/components/Cards/consts/cardItems.jsx
+++ src/components/Cards/consts/cardItems.jsx
@@ -1,6 +1,44 @@
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import exaSPIM from '../../../images/AIND_exaspim.jpg';
 import allenHQ from '../../../images/allen-institute-hq.jpg';
 import astrocytes from '../../../images/astrocytes_and_astrocyte_morphology.jpg';
+
+const dataCard = (
+  <>
+
+    <Typography paragraph variant="body1" color="primary">
+      <Link href="https://codeocean.allenneuraldynamics.org/">
+        Code Ocean
+      </Link>
+    </Typography>
+
+    <Typography paragraph variant="body1" color="primary">
+      <Link href="https://dandiarchive.org/">
+        DANDI Archive
+      </Link>
+    </Typography>
+
+    <Typography paragraph variant="body1" color="primary">
+      <Link href="https://github.com/allenNeuralDynamics">
+        GitHub
+      </Link>
+    </Typography>
+
+    <Typography paragraph variant="body1" color="primary">
+      <Link href="https://www.protocols.io/workspaces/allen-institute-for-neural-dynamics">
+        Protocols.io
+      </Link>
+    </Typography>
+
+    <Typography paragraph variant="body1" color="primary">
+      <Link href="https://osf.io/">
+        Open Science Framework
+      </Link>
+    </Typography>
+
+  </>
+);
 
 const cards = [
   {
@@ -18,9 +56,9 @@ const cards = [
   {
     id: 3,
     title: 'Data',
-    description: 'Protocols.io, OSF, GitHub',
+    description: 'Protocols.io, OSF, GitHub, etc',
     image: exaSPIM,
-    content: 'Protocols.io',
+    content: dataCard,
   },
 ];
 
